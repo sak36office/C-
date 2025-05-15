ef45d3a08dbd1f3cd09dec26f8ed162ab5611a89
sak36office <153987774+sak36office@users.noreply.github.com>
2025-05-15 19:43:20 +0700

    Create lab01DataType.cpp
    001

---
+++ lab01DataType.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    // Data Type and Variable
+    list<int> digit = {0,3,5,7,8,9,9,2,8};
+    string name = "tanachot";
+    string email = "tanachotcallsak36office@gmail.com";
+    string phone = "0935789928";
+    char blood = 'A';
+
+    double gpa = 3.141592;
+    float info = 1.414;
+    int age = 50;
+
+    bool isActive = true;
+
+    vector<char> alph = {'t','a','n','a','c','h','o','t'};
+
+    
+    
+
+
+
+    return 0;
+}
